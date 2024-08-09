42c836b378f0884da3cd1208e7ea3abb7c0fc845
Fernando del Pino <fherdelpino@hotmail.com>
2024-08-09 16:25:09 -0600

    Initial Commit

---
+++ Live Metronome/Live_MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_MetronomeApp.swift
+//  Live Metronome
+//
+//  Created by Fernando del Pino on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Live_MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
