de8249854a89d249a617445912f01dd966b8d566
Fernando del Pino <fherdelpino@hotmail.com>
2024-08-12 11:33:25 -0600

    Live Metonome first iteration

--- Live Metronome/ContentView.swift
+++ Live Metronome/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor: Color = .white
+    @State private var time = ProcessInfo.processInfo.systemUptime
+    @State private var bpm = 0
+    @State private var diff = 0.0
+    @State private var flag: Bool = false
+    @State private var scale: CGFloat = 1.0
+    @State private var optionalTimer: Timer?
+    
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("BPM \(bpm)")
+            .font(.largeTitle)
+            /*Circle()
+            .fill(Color.red)
+            .frame(width: 100, height: 100)
+            .scaleEffect(scale)
+            .animation(Animation.easeInOut(duration: diff).repeatForever(autoreverses: true))
+            .onAppear {
+                startPulsating()
+            }*/
+            
+            Button("Tap") {
+                let tmp = ProcessInfo.processInfo.systemUptime
+                diff = tmp-time
+                bpm = Int(60/diff)
+                time = tmp
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+            
+            Button("Start") {
+                if let timer = optionalTimer {
+                    timer.invalidate()
+                }
+                optionalTimer = Timer.scheduledTimer(withTimeInterval: diff, repeats: true) { _ in
+                    timerJob()
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+            
+            Button("Stop") {
+                if let timer = optionalTimer {
+                    timer.invalidate()
+                }
+                backgroundColor = .white
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            backgroundColor.ignoresSafeArea()
+        }
+        
+    }// end body
+    
+    func timerJob() {
+        if (flag) {
+            backgroundColor = .white
+            flag = false
+        } else{
+            backgroundColor = .red
+            flag = true
+        }
+    }
+    
+    func startPulsating() {
+        withAnimation {
+            scale = 1.5
+        }
     }
 }
 
