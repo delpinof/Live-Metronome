34cd133dc4c40163394c396d200283c5c1ffa64d
Fernando del Pino <fherdelpino@hotmail.com>
2024-08-13 09:02:19 -0600

    Improve UI (GREEN) (#2)
    * Add icon and change button colors
    
    * add ui improvements and color change

--- Live Metronome/ContentView.swift
+++ Live Metronome/ContentView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor: Color = .white
+    private let BUTTON_COLOR: Color = .green
+    private let BACKGROUND_COLOR: Color = .black
+    @State private var backgroundColor: Color = .black
     @State private var time = ProcessInfo.processInfo.systemUptime
     @State private var bpm = 120
     @State private var flag: Bool = false
@@ -19,13 +21,15 @@ struct ContentView: View {
         
         VStack {
             
-            HStack{
+            VStack{
                 Text("BPM")
                 TextField("", value: $bpm, format: .number)
                     .keyboardType(.numberPad)
                     .focused($textFieldFocused)
             }
             .font(.largeTitle)
+            .multilineTextAlignment(.center)
+            .foregroundColor(BUTTON_COLOR)
             
             Button("Tap") {
                 let tmp = ProcessInfo.processInfo.systemUptime
@@ -34,7 +38,7 @@ struct ContentView: View {
                 textFieldFocused = false
             }
             .buttonStyle(.borderedProminent)
-            .tint(.green)
+            .tint(BUTTON_COLOR)
             
             Button("Start") {
                 let interval = BpmConverter.getInterval(from: bpm)
@@ -47,17 +51,17 @@ struct ContentView: View {
                 textFieldFocused = false
             }
             .buttonStyle(.borderedProminent)
-            .tint(.green)
+            .tint(BUTTON_COLOR)
             
             Button("Stop") {
                 if let timer = optionalTimer {
                     timer.invalidate()
                 }
-                backgroundColor = .white
+                backgroundColor = BACKGROUND_COLOR
                 textFieldFocused = false
             }
             .buttonStyle(.borderedProminent)
-            .tint(.green)
+            .tint(BUTTON_COLOR)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background {
@@ -68,10 +72,10 @@ struct ContentView: View {
     
     func timerJob() {
         if (flag) {
-            backgroundColor = .white
+            backgroundColor = BACKGROUND_COLOR
             flag = false
         } else{
-            backgroundColor = .red
+            backgroundColor = BUTTON_COLOR
             flag = true
         }
     }
